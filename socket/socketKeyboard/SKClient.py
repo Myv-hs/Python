1ffd3302e6a26abb4940ae8594dede4304b011d8
matthew <matthewcoyle@me.com>
2017-08-17 03:51:18 +0200

    Socket Keyboard Removed key checks on client side for ? ^ and windows Added exceptions for keys on server side Added possibility to choose server with arguments

--- socket/socketKeyboard/SKClient.py
+++ socket/socketKeyboard/SKClient.py
@@ -1,9 +1,15 @@
+import sys
 import socket               
 import keyboard
 
 trgtIP = '192.168.1.9'
 port = 12346
 toggle = False
+
+if len(sys.argv)>1:
+	inputIP = sys.argv[1]
+	trgtIP = inputIP
+	print("You've chosen to connect to: "+trgtIP)
 
 def sendKey (e):
 	global toggle
@@ -15,11 +21,14 @@ def sendKey (e):
 		toggle = not toggle
 		print("Key Transfer on: "+str(toggle))
 
-	if toggle == True and e.name !='esc' and e.name !='?' and e.name !='windows' and e.name !='asciicircum':
+	if toggle == True and e.name !='esc':# and e.name !='?' and e.name !='asciicircum':
 		keyname = e.name
 		key = e.event_type+" "+keyname
-		s.connect((trgtIP, port))
-		s.send(key.encode())
+		try:
+			s.connect((trgtIP, port))
+			s.send(key.encode())
+		except:
+			print("Socket failed, check input IP")
 
 keyboard.hook(sendKey)
 keyboard.wait()
--- socket/socketKeyboard/SKServer.py
+++ socket/socketKeyboard/SKServer.py
@@ -1,57 +1,38 @@
-# first of all import the socket library
 import socket
 import keyboard     
 
 def down(key):
-	if not keyboard.is_pressed(key):
-		keyboard.press(key)
-		#keyboard.press_and_release(key)
+	try:
+		if not keyboard.is_pressed(key):
+			keyboard.press(key)
+	except:
+		print("Key Invalid")
 
 def up(key):
-	if keyboard.is_pressed(key):
-		keyboard.release(key)
+	try:
+		if keyboard.is_pressed(key):
+			keyboard.release(key)
+	except:
+		print("Invalid Key released")
 
-# next create a socket object
 s = socket.socket()         
-print("Socket successfully created")
-
-# reserve a port on your computer in our
-# case it is 12346 but it can be anything
 port = 12346                
 
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests 
-# coming from other computers on the network
 s.bind(('', port))        
 print("socket binded to %s" %(port))
-
-# put the socket into listening mode
 s.listen(5)     
 print("socket is listening")            
 
-# a forever loop until we interrupt it or 
-# an error occurs
 while True:
-	# Establish connection with client.
 	c, addr = s.accept()     
 	print('Got keyevent from', addr)
 
-	# send a thank you message to the client.
-	#sndmsg = "Hello Keyboard"
-	#sndmsg2 = "anothermsg"
-	#c.send(sndmsg.encode())
-	#c.send(sndmsg2.encode())
-	# Close the connection with the client
-	#laststring = ''
 	instring = c.recv(4096).decode()
 	#instring = int(instring)
 	#keyboard.press(instring)
 	if instring:
 		print(type(instring))
 		print(instring)
-		#down(instring)
 		if len(instring.split(' '))>1:
 			eventtype = instring.split(' ')[0]
 			if eventtype == 'down':
