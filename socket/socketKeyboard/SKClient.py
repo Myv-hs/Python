c991eb3ca11ab84b2e845a733e8c84f5f5f990da
matthew <matthewcoyle@me.com>
2017-09-05 19:40:34 +0200

    SKC keybind update

--- socket/socketKeyboard/SKClient.py
+++ socket/socketKeyboard/SKClient.py
@@ -5,6 +5,7 @@ import keyboard
 trgtIP = '192.168.1.9'
 port = 12346
 toggle = False
+toggleKey = 'delete'
 
 if len(sys.argv)>1:
 	inputIP = sys.argv[1]
@@ -17,18 +18,18 @@ def sendKey (e):
 	global trgtIP
 	s = socket.socket()         
 
-	if e.name == 'esc' and e.event_type == 'down':
+	if e.name == toggleKey and e.event_type == 'down':
 		toggle = not toggle
 		print("Key Transfer on: "+str(toggle))
 
-	if toggle == True and e.name !='esc':# and e.name !='?' and e.name !='asciicircum':
+	if toggle == True and e.name !=toggleKey:
 		keyname = e.name
 		key = e.event_type+" "+keyname
 		try:
 			s.connect((trgtIP, port))
 			s.send(key.encode())
 		except:
-			print("Socket failed, check input IP")
+			print("Socket failed, check input IP is correct\nand target SKServer.py is ready")
 
 keyboard.hook(sendKey)
 keyboard.wait()
